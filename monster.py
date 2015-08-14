de638bf2623bb50eaf0a3b9be9126d690f8e5610
Charles Neill <charles.neill@rackspace.com>
2015-08-14 16:29:52 -0500

    gogogogogo

--- monster.py
+++ monster.py
@@ -2,24 +2,34 @@
 """MONSTER
 
 Usage:
-    monster.py
+    monster.py spider URL OUTFILE
+    monster.py attack URL INFILE [--fuzz_types=<ft>]
+    monster.py rampage URL OUTFILE
     monster.py --help
-    monster.py attack --fuzz_types=<ft> URL
+
 
 Options:
+    spider                 SPIDER!
+    attack                 ATTACK!
+    rampage                ATTACK + SPIDER!
     --help                 Show this help message
     --fuzz_types=<ft>      Comma-separated list of types of fuzz tests to run
 
 """
-
-# import unittest
+import json
 
 from docopt import docopt
 from selenium import webdriver
 
+from tests import XSSTest
+
 
 SET_ATTR_SCRIPT = """var elem = document.querySelector('{selector}');
 elem.{attr} = {val};
+"""
+
+REMOVE_ATTR_SCRIPT = """var elem = document.querySelector('{selector}');
+elem.removeAttribute("{attr}");
 """
 
 
@@ -34,19 +44,32 @@ class MonsterClient(object):
 
     def get_elem_attrs(self, elem):
         attr_names = [
-            'action', 'class', 'href', 'id', 'maxlength', 'name', 'type'
+            'action', 'class', 'href', 'id', 'maxlength', 'name', 'type',
+            'disabled', 'max', 'min', 'pattern', 'required', 'steps'
         ]
         attrs = {}
         for name in attr_names:
             attrs[name] = elem.get_attribute(name)
         return attrs
 
+    def get_elem_children(self, elem):
+        return elem.find_elements_by_css_selector('*')
+
+    def get_elem_by_selector(self, selector, parent=None):
+        try:
+            if parent:
+                return parent.find_element_by_css_selector(selector)
+            else:
+                return self.driver.find_element_by_css_selector(selector)
+        except:
+            print "NO ELEMENT FOUND"
+
     def get_best_selector(self, elem):
         attrs = self.get_elem_attrs(elem)
         if attrs['id']:
             return '#{0}'.format(attrs['id'])
         elif attrs['name']:
-            return 'name={0}'.format(attrs['name'])
+            return '[name={0}]'.format(attrs['name'])
         elif attrs['class']:
             return '.{0}'.format(attrs['class'])
         elif elem.tag_name:
@@ -56,22 +79,25 @@ class MonsterClient(object):
 
     def set_elem_attr(self, elem, attr, val):
         selector = self.get_best_selector(elem)
-        if selector:
-            self.set_elem_attr_by_selector(selector, attr, val)
-        else:
-            raise Exception("Couldn't find selector")
+        self.set_elem_attr_by_selector(selector, attr, val)
 
     def set_elem_attr_by_selector(self, selector, attr, val):
         self.driver.execute_script(SET_ATTR_SCRIPT.format(
             selector=selector, attr=attr, val=val
         ))
 
+    def remove_elem_attr(self, elem, attr):
+        selector = self.get_best_selector(elem)
+        self.driver.execute_script(REMOVE_ATTR_SCRIPT.format(
+            selector=selector, attr=attr
+        ))
+
 
 class MonsterSpider(MonsterClient):
 
-    def __init__(self, url):
+    def __init__(self, url, outfile):
         super(MonsterSpider, self).__init__(url)
-        self.get(self.base_url)
+        self.outfile = outfile
 
     def get_forms(self, forms=[], depth=0, max_depth=4):
         if depth < max_depth:
@@ -85,21 +111,49 @@ class MonsterSpider(MonsterClient):
         curr_url = self.base_url
 
         if depth < max_depth:
-            for button in self.get_buttons():
-                button.click()
+            buttons = self.get_buttons()
+            button_selectors = []
+            for button in buttons:
+                button_selectors.append(self.get_best_selector(button))
+
+            links = self.get_links()
+            link_selectors = []
+            for link in links:
+                link_selectors.append(self.get_best_selector(link))
+
+            for selector in button_selectors:
+                button_elem = self.get_elem_by_selector(selector)
+                button_elem.click()
                 if self.driver.current_url != curr_url:
                     self.driver.back()
                 else:
                     forms.extend(self.get_forms(forms, depth=depth+1))
 
-            for link in self.get_links():
-                link.click()
+            for selector in link_selectors:
+                link_elem = self.get_elem_by_selector(selector)
+                link_elem.click()
                 if self.driver.current_url != curr_url:
                     self.driver.back()
                 else:
                     forms.extend(self.get_forms(forms, depth=depth+1))
 
             return list(set(forms))
+
+    def get_webelem_obj(self, elem):
+        children = []
+        attrs = self.get_elem_attrs(elem)
+        children_elems = self.get_elem_children(elem)
+
+        for child in children_elems:
+            children.append(self.get_webelem_obj(child))
+
+        return {
+            'tag_name': elem.tag_name,
+            'name': attrs['name'] or "UNNAMED OBJECT",
+            'selector': self.get_best_selector(elem),
+            'attrs': attrs,
+            'children': children
+        }
 
     def get_buttons(self):
         return self.driver.find_elements_by_tag_name('button')
@@ -111,80 +165,93 @@ class MonsterSpider(MonsterClient):
         return form.find_elements_by_tag_name('input')
 
     def spider(self):
-        self.get(self.base_url)
-        forms = self.get_forms()
-        links = self.get_links()
+        forms = []
+        links = []
 
-        return {
+        self.get(self.base_url)
+
+        form_elems = self.get_forms()
+        link_elems = self.get_links()
+
+        for elem in form_elems:
+            forms.append(self.get_webelem_obj(elem))
+
+        for elem in link_elems:
+            links.append(self.get_webelem_obj(elem))
+
+        self.driver.close()
+
+        result = {
             'base_url': self.base_url,
             'forms': forms,
             'links': links,
         }
 
+        with open(self.outfile, 'w') as f:
+            f.write(json.dumps(result, sort_keys=True, indent=4))
+
 
 class MonsterAttacker(MonsterClient):
 
     def __init__(self, spider_log):
-        """
-        self.driver = spider_log['driver']
-        """
-        super(MonsterAttacker, self)
+        with open(spider_log, 'r') as f:
+            self.spider_log = json.loads(f.read())
+        self.base_url = self.spider_log['base_url']
+        super(MonsterAttacker, self).__init__(self.base_url)
         self.attacks = {
-            'xss': XSSTest()
+            'xss': XSSTest(self, self.base_url)
         }
 
     def go(self):
-        print 'ha'
+        self.get(self.base_url)
 
-    def neuter_input(self, elem):
-        attrs = self.get_cleanup_attrs(elem)
+        for form in self.spider_log['forms']:
+            form_elem = self.get_elem_by_selector(form['selector'])
+            if form_elem:
+                form_children = self.get_elem_children(form_elem)
+                for child in form_children:
+                    self.remove_elem_validation(child)
+
+                self.attack_form(form_elem)
+
+    def remove_elem_validation(self, elem):
+        attrs = self.get_elem_attrs(elem)
         bad_types = [
             'color', 'date', 'datetime', 'datetime-local', 'email', 'month',
             'number', 'range', 'search', 'tel', 'time', 'url', 'week'
         ]
-        if attrs['type']:
-            if attrs['type'] in bad_types:
-                self.set_elem_attr(elem, 'type', '"text"')
-        if attrs['maxlength']:
-            self.set_elem_attr(elem, 'maxLength', 5000)
+        attrs_to_remove = [
+            'disabled', 'required', 'max', 'min', 'pattern', 'maxlength'
+        ]
+        if elem.tag_name == 'input':
+            for attr in attrs_to_remove:
+                if attrs[attr]:
+                    if attr == 'maxlength':
+                        self.remove_elem_attr(elem, 'maxLength')
+                    else:
+                        self.remove_elem_attr(elem, attr)
 
+            if attrs['type']:
+                if attrs['type'] in bad_types:
+                    self.set_elem_attr(elem, 'type', '"text"')
 
-class MonsterTest(MonsterClient):
-    data = {}
-
-    def __init__(self, spider_log):
-        print 'hahahha'
-
-    def get_strings(self):
-        return self.data.values()
-
-    def verify_response(self):
-        print 'ha'
-
-
-class XSSTest(MonsterTest):
-
-    data = {
-        'double_bracket': '<<script>alert(1);//<</script>',
-        'tag_close': '\'"><script>alert(1);</script>',
-        'img_js_link': '<IMG SRC=javascript:alert(1)>',
-        'img_js_link_w_0x0D': '<IMG SRC=jav&#x0D;ascript:alert(1);>',
-        'img_js_link_overencode':
-            "<IMG%20SRC='%26%23x6a;avasc%26%23000010ript:alert(1);'>",
-        'iframe_js_link': '<IFRAME SRC=javascript:alert(1)></IFRAME>',
-        'js_context': '\\\'";alert(1);//'
-    }
-
-    def verify_response():
-        print 'ha'
+    def attack_form(self, elem):
+        for attack in self.attacks:
+            self.attacks[attack].attack(elem)
 
 
 def main(args):
-    ms = MonsterSpider('http://localhost:8081/test_site.html')
-    forms = ms.get_forms()
-    for form in forms:
-        print form.get_attribute('name')
-        print form.find_elements_by_tag_name('input')
+    if args['attack']:
+        ma = MonsterAttacker(args['INFILE'])
+        ma.go()
+    elif args['spider']:
+        ms = MonsterSpider(args['URL'], args['OUTFILE'])
+        ms.spider()
+    elif args['rampage']:
+        ms = MonsterSpider(args['URL'], args['OUTFILE'])
+        ms.spider()
+        ma = MonsterAttacker(args['OUTFILE'])
+        ma.go()
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='0.0')
